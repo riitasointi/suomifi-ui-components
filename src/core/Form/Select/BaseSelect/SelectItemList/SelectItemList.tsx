66a6bd0f130d0897d2f04d3a8bf5507c9300e90f
Ilkka Pättikangas <ilkka.pattikangas@gmail.com>
2022-05-13 13:37:05 +0300

    WIP: Comment out redundant imports for now

--- src/core/Form/Select/BaseSelect/SelectItemList/SelectItemList.tsx
+++ src/core/Form/Select/BaseSelect/SelectItemList/SelectItemList.tsx
@@ -10,10 +10,10 @@ import { default as styled } from 'styled-components';
 import classnames from 'classnames';
 import { SuomifiThemeProp, SuomifiThemeConsumer } from '../../../../theme';
 import { HtmlDivWithRef, HtmlUlWithRef } from '../../../../../reset';
-import { SelectItemProps } from '../SelectItem/SelectItem';
+// import { SelectItemProps } from '../SelectItem/SelectItem';
 import { baseStyles } from './SelectItemList.baseStyles';
-import { SelectEmptyItemProps } from '../SelectEmptyItem/SelectEmptyItem';
-import { SelectSuggestionProps } from '../SelectSuggestion/SelectSuggestion';
+// import { SelectEmptyItemProps } from '../SelectEmptyItem/SelectEmptyItem';
+// import { SelectSuggestionProps } from '../SelectSuggestion/SelectSuggestion';
 
 const baseClassName = 'fi-select-item-list';
 
